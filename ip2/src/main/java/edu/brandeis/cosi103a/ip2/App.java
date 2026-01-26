c81736f98758463f21d34f75033b27b06e3b752a
nayena <heidynaranjo@brandeis.edu>
2026-01-26 14:06:48 -0500

    build: main  interface

---
+++ ip2/src/main/java/edu/brandeis/cosi103a/ip2/App.java
@@ -0,0 +1,13 @@
+package edu.brandeis.cosi103a.ip2;
+
+/**
+ * Main entry point for Automation: The Game.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("=== Automation: The Game ===\n");
+        Game game = new Game();
+        game.play();
+    }
+}
+
